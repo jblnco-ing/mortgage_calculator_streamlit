36ed01cf9243c65e9d72db180470e7f4944bd666
Joel Blanco <jblnco.ing@gmail.com>
2025-12-09 07:10:54 -0300

    feat: add streamlit hello world

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+def main() -> None:
+    """Simple hello world app."""
+    st.title("Hola, Streamlit")
+    st.write("¡Hola mundo!")
+
+
+if __name__ == "__main__":
+    main()
+
